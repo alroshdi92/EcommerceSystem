d9b3f0d890f63bd9eaebe2d90434df6a69a5eb23
Hajer-Alroshdi <alroshdi992@gmail.com>
2023-06-05 00:23:30 +0400

    commit

--- database/migrations/2023_06_03_171707_create_catogries_table.php
+++ database/migrations/2023_06_03_171707_create_catogries_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('catogries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedInteger('user_id');
             $table->timestamps();
         });
     }
